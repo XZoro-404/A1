8e46266e01d375070d274013f60cd3d18cd419cf
osc <myemail@testemail.com>
2022-10-18 15:55:02 -0600

    This has two variables arraySize which is the how many numbers are in the children file number which is all of the numbers in the children file

--- A1.c
+++ A1.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int * getChildren(int parent) {
-    parent = ("/proc/%i/task/%i/children", parent);
-    FILE *in_file = fopen("/proc/%i/task/%i/children, parent", "r"); // read only
-    int number;
+
+    char parentNum[20] = ""; // the int that gets passed in
+    char filePath[100] = ""; // the string for the file path
+    // Below we are changing the parent int to a string and creating the file path string
+    sprintf(parentNum, "%d", parent);
+    strcat(filePath, "/proc/");
+    strcat(filePath, parentNum);
+    strcat(filePath, "/task/");
+    strcat(filePath, parentNum);
+    strcat(filePath, "/children");
+
+//    printf("Here is the file location: %s", filePath); // you can uncomment this to debug the filepath
+    FILE *in_file = fopen(filePath, "r"); // read only
+    int number; // this is the numbers in the children file
     int arraySize = 0;
 
     // test for files not existing.
@@ -12,14 +24,13 @@ int * getChildren(int parent) {
         printf("Error! Could not open file\n");
         exit(-1); // must include stdlib.h
     }
-
+    // goes through the file and gets the numbers while counting the amount of ints in the file
     while (fscanf(in_file, "%d", &number ) == 1)
     {
         arraySize++;
-        printf("number: %d\n", arraySize);
-
-
+        printf("number: %d\n", number);
     }
+    printf("final number: %d", arraySize);
     fclose(in_file);
 }
 
